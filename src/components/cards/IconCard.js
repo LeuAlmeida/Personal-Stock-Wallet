80680ef487780aacfb32ec3d6b80beb712513a41
Léu Almeida <leo@webid.net.br>
2019-09-11 15:02:51 -0300

    Update IconCard.js

--- src/components/cards/IconCard.js
+++ src/components/cards/IconCard.js
@@ -7,7 +7,7 @@ const IconCard = ({className="mb-4", icon, title, value }) => {
     <div className={`icon-row-item ${className}`}>
       <Card >
         <CardBody className="text-center">
-          <i className={icon} />
+          <i className={`iconsminds-${icon}`} />
           <p className="card-text font-weight-semibold mb-0">
             <IntlMessages id={title} />
           </p>
