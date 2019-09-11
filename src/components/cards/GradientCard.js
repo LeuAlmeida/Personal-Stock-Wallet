c87722aea1ad9ea0ad588dcbe27281ec36f1cf60
Léu Almeida <leo@webid.net.br>
2019-09-11 15:02:43 -0300

    Update GradientCard.js

--- src/components/cards/GradientCard.js
+++ src/components/cards/GradientCard.js
@@ -4,9 +4,9 @@ import { Card, CardBody } from "reactstrap";
 const GradientCard = ({ children }) => {
   return (
     <Card className="dashboard-sq-banner justify-content-end">
-      <CardBody className="justify-content-end d-flex flex-column">
-        {children}
-      </CardBody>
+        <CardBody className="justify-content-end d-flex flex-column">
+          {children}
+        </CardBody>
     </Card>
   );
 };
