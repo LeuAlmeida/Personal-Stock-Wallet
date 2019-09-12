7aab9bb3f6b28e3766b817a5085d2d5d331a4987
Léu Almeida <leo@webid.net.br>
2019-09-12 16:27:37 -0300

    Update second.js

--- src/views/app/second-menu/second.js
+++ src/views/app/second-menu/second.js
@@ -16,11 +16,31 @@ export default class Second extends Component {
                 <Separator className="mb-5" />
               </Colxx>
             </Row>
-            <Row>
-              <Colxx xxs="4" className="mb-4">
-                <GradientWithRadialProgressCard icon="bank">
 
-                </GradientWithRadialProgressCard>
+            <Row>
+              <Colxx xxs="12">
+                <div className="react-contextmenu-wrapper">
+                  <div className="d-flex flex-row active card">
+                    <div className="pl-2 d-flex flex-grow-1 min-width-zero">
+                      <div className="card-body align-self-center d-flex flex-column flex-lg-row justify-content-between min-width-zero align-items-lg-center">
+                        <a className="w-40 w-sm-100" href="https://clear.com.br">
+                          <p className="list-item-heading mb-1 truncate">Clear Corretora</p>
+                        </a>
+                        <p className="mb-1 text-muted text-small w-15 w-sm-100">Desserts</p>
+                        <p className="mb-1 text-muted text-small w-15 w-sm-100">27.01.2019</p>
+                        <div className="w-15 w-sm-100">
+                          <span className="badge badge-secondary badge-pill">PROCESSED</span>
+                        </div>
+                      </div>
+                      <div className="custom-control custom-checkbox pl-1 align-self-center pr-4">
+                        <div className="item-check mb-0 custom-checkbox custom-control">
+                          <input type="checkbox" id="check_18" className="custom-control-input" />
+                            <label className="custom-control-label" for="check_18"></label>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
               </Colxx>
             </Row>
           </Fragment>
