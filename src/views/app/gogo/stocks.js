68eb326c091e4b496f0f2ddda51dce08b33897ae
Léu Almeida <leo@webid.net.br>
2019-09-12 19:11:14 -0300

    Update stocks.js

--- src/views/app/gogo/stocks.js
+++ src/views/app/gogo/stocks.js
@@ -1,24 +1,110 @@
 import React, { Component, Fragment } from "react";
 import { Row } from "reactstrap";
-import IntlMessages from "../../../helpers/IntlMessages";
 import { Colxx, Separator } from "../../../components/common/CustomBootstrap";
 import Breadcrumb from "../../../containers/navs/Breadcrumb";
- 
+import '../../../assets/css/common/style.css';
+
+import Tabletop from 'tabletop';
+
 export default class Stocks extends Component {
+  constructor() {
+    super()
+    this.state = {
+      data: []
+    }
+  }
+
+  componentDidMount() {
+    Tabletop.init({
+      key: '1L-ywhumD0TnbJVxhMK_zZ9hnPsSKA86xPen1XCqNuG8',
+      orderby: 'Posição',
+      reverse: true,
+      wanted: ['Stocks'],
+
+      callback: googleData => {
+        this.setState({
+          data: googleData,
+        })
+
+      },
+      simpleSheet: true
+    })
+  }
+
     render() {
+      const { data } = this.state
         return (
             <Fragment>
             <Row>
-              <Colxx xxs="12">
+              <Colxx xxs="10">
                 <Breadcrumb heading="menu.stocks" match={this.props.match} />
-                <Separator className="mb-5" />
+              </Colxx>
+              <Colxx xxs="2">                
+                {data.map(obj => {
+                  return (
+                    <span key={obj.id}>
+                      <h1>{obj.Total}</h1>
+                    </span>
+                  )
+                })}
+              </Colxx>
+              <Colxx xxs="12">
+              <Separator className="mb-5" />
               </Colxx>
             </Row>
+
             <Row>
-              <Colxx xxs="12" className="mb-4">
-                <p><IntlMessages id="menu.stocks"/></p>
+              <Colxx xxs="12">
+                          
+                          <div className="pl-2 d-flex flex-grow-1 min-width-zero">
+                          <div className="card-body align-self-center d-flex flex-column flex-lg-row justify-content-between min-width-zero align-items-lg-center">
+                          <p className="list-item-heading mb-1 truncate">Marca</p>
+                            <p className="list-item-heading mb-1 truncate">Empresa</p>
+                            <p className="mb-1 text-small w-15 w-sm-100">Ticker</p>
+                            <p className="mb-1 text-muted text-small w-15 w-sm-100">Cotação</p>
+                            <p className="mb-1 text-small w-15 w-sm-100">Posição</p>
+                            <p className="mb-1 text-muted text-small w-15 w-sm-100">Lucro</p>
+                            <p className="mb-1 text-muted text-small w-15 w-sm-100">P/M</p>
+                            <p className="mb-1 text-muted text-small w-15 w-sm-100">Rentabilidade</p>
+                            <p className="mb-1 text-muted text-small w-15 w-sm-100">Falta</p>
+                            <p className="mb-1 text-muted text-small w-15 w-sm-100">Situação</p>
+                          </div>
+                        </div>
               </Colxx>
             </Row>
+
+            <Row>
+              {
+                  data.map(obj => {
+                    return (
+                      <Colxx xs="12" className="mb-4" key={obj.id}>
+
+                        <div className="d-flex flex-row active card">
+                          <img alt={obj.Nome} src={obj.Logo} className="list-thumbnail responsive border-0 card-img-left" />
+                        <div className={'pl-2 d-flex flex-grow-1 min-width-zero'}>
+                          <div className="card-body align-self-center d-flex flex-column flex-lg-row justify-content-between min-width-zero align-items-lg-center">
+                              <p className="list-item-heading mb-1 truncate">{obj.Nome}</p>
+                            <p className="mb-1 text-small w-15 w-sm-100">{obj.Ticker}</p>
+                            <p className="mb-1 text-muted text-small w-15 w-sm-100">{obj.Preço}</p>
+                            <p className="mb-1 text-small w-15 w-sm-100">{obj.Posição}</p>
+                            <p className="mb-1  text-small w-15 w-sm-100">{obj.Lucro}</p>
+                            <p className="mb-1 text-muted text-small w-15 w-sm-100">{obj.PM}</p>
+                            <p className="mb-1 text-muted text-small w-15 w-sm-100">{obj.Rentabilidade}</p>
+                            <p className="mb-1 text-muted text-small w-15 w-sm-100">{obj.Falta}</p>
+                            <h4 className="w-15 w-sm-100">
+                              <a href={obj.FaltaCotas >= 0 ? 'http://clear.com.br' : 'javascript:()'} target="_blank">
+                                <span className={`badge badge-${obj.Situação == 'Comprar' ? 'success' : 'outline-danger'} badge-pill`}>{obj.Situação}
+                                </span>
+                                </a>
+                            </h4>
+                          </div>
+                        </div>
+                        </div>
+                      </Colxx>
+                    )
+                  })
+                }
+              </Row>
           </Fragment>
         )
     }
