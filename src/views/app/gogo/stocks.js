38458010c3ca6cfa5af6454a45f77a7f0cfa90bd
Léu Almeida <leo@webid.net.br>
2019-09-13 16:29:34 -0300

    Update stocks.js

--- src/views/app/gogo/stocks.js
+++ src/views/app/gogo/stocks.js
@@ -58,16 +58,36 @@ export default class Stocks extends Component {
                           
                           <div className="pl-2 d-flex flex-grow-1 min-width-zero">
                           <div className="card-body align-self-center d-flex flex-column flex-lg-row justify-content-between min-width-zero align-items-lg-center">
-                          <p className="list-item-heading mb-1 truncate">Marca</p>
-                            <p className="list-item-heading mb-1 truncate">Empresa</p>
-                            <p className="mb-1 text-small w-15 w-sm-100">Ticker</p>
-                            <p className="mb-1 text-muted text-small w-15 w-sm-100">Cotação</p>
-                            <p className="mb-1 text-small w-15 w-sm-100">Posição</p>
-                            <p className="mb-1 text-muted text-small w-15 w-sm-100">Lucro</p>
-                            <p className="mb-1 text-muted text-small w-15 w-sm-100">P/M</p>
-                            <p className="mb-1 text-muted text-small w-15 w-sm-100">Rentabilidade</p>
-                            <p className="mb-1 text-muted text-small w-15 w-sm-100">Falta</p>
-                            <p className="mb-1 text-muted text-small w-15 w-sm-100">Situação</p>
+                          <p className="list-item-heading mb-1 truncate">
+                            Marca
+                          </p>
+                          <p className="list-item-heading mb-1 truncate">
+                            Empresa
+                          </p>
+                          <p className="list-item-heading mb-1 truncate">
+                            Ticker
+                          </p>
+                          <p className="mb-1 text-muted text-small w-15 w-sm-100">
+                            Preço
+                          </p>
+                          <p className="mb-1 text-muted text-small w-15 w-sm-100">
+                            Posição
+                          </p>
+                          <p className="mb-1 text-muted text-small w-15 w-sm-100">
+                            Lucro
+                          </p>
+                          <p className="mb-1 text-muted text-small w-15 w-sm-100">
+                            P/M
+                          </p>
+                          <p className="mb-1 text-muted text-small w-15 w-sm-100">
+                            Rentab.
+                          </p>
+                          <p className="mb-1 text-muted text-small w-15 w-sm-100">
+                            Falta
+                          </p>
+                          <p className="mb-1 text-muted text-small w-15 w-sm-100">
+                            Situação
+                          </p>
                           </div>
                         </div>
               </Colxx>
@@ -92,7 +112,7 @@ export default class Stocks extends Component {
                             <p className="mb-1 text-muted text-small w-15 w-sm-100">{obj.Rentabilidade}</p>
                             <p className="mb-1 text-muted text-small w-15 w-sm-100">{obj.Falta}</p>
                             <h4 className="w-15 w-sm-100">
-                              <a href={obj.FaltaCotas >= 0 ? 'http://clear.com.br' : 'javascript:()'} target="_blank">
+                              <a href={obj.FaltaCotas >= 0 ? 'https://you.drivewealth.com' : '#'} target={obj.FaltaCotas >= 0 ? '_blank' : '_self'}>
                                 <span className={`badge badge-${obj.Situação == 'Comprar' ? 'success' : 'outline-danger'} badge-pill`}>{obj.Situação}
                                 </span>
                                 </a>
