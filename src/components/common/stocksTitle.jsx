017946f1d6ce4d38b2fce87e5807ee1773fb2e29
Léu Almeida <leo@webid.net.br>
2019-09-13 20:11:42 -0300

    Create stocksTitle.jsx

---
+++ src/components/common/stocksTitle.jsx
@@ -0,0 +1,38 @@
+import React from 'react'
+
+export default props => (
+    <div className="pl-2 d-flex flex-grow-1 min-width-zero">
+                          <div className="card-body align-self-center d-flex flex-column flex-lg-row justify-content-between min-width-zero align-items-lg-center">
+                          <p className="list-item-heading mb-1 truncate">
+                            Marca
+                          </p>
+                          <p className="list-item-heading mb-1 truncate">
+                            Empresa
+                          </p>
+                          <p className="list-item-heading mb-1 truncate">
+                            Ticker
+                          </p>
+                          <p className="mb-1 text-muted text-small w-15 w-sm-100">
+                            Preço
+                          </p>
+                          <p className="mb-1 text-muted text-small w-15 w-sm-100">
+                            Posição
+                          </p>
+                          <p className="mb-1 text-muted text-small w-15 w-sm-100">
+                            Lucro
+                          </p>
+                          <p className="mb-1 text-muted text-small w-15 w-sm-100">
+                            P/M
+                          </p>
+                          <p className="mb-1 text-muted text-small w-15 w-sm-100">
+                            Rentab.
+                          </p>
+                          <p className="mb-1 text-muted text-small w-15 w-sm-100">
+                            Falta
+                          </p>
+                          <p className="mb-1 text-muted text-small w-15 w-sm-100">
+                            Situação
+                          </p>
+                        </div>
+                        </div>
+)
