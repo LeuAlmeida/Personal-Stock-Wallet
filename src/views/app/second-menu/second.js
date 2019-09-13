4b6de66e6e48ebdf3289a154e609a8e93e8ee7f9
Léu Almeida <leo@webid.net.br>
2019-09-13 16:29:40 -0300

    Update second.js

--- src/views/app/second-menu/second.js
+++ src/views/app/second-menu/second.js
@@ -1,48 +1,107 @@
 import React, { Component, Fragment } from "react";
 import { Row } from "reactstrap";
-// import IntlMessages from "../../../helpers/IntlMessages";
 import { Colxx, Separator } from "../../../components/common/CustomBootstrap";
 import Breadcrumb from "../../../containers/navs/Breadcrumb";
+import '../../../assets/css/common/style.css';
 import GradientWithRadialProgressCard from "../../../components/cards/GradientWithRadialProgressCard";
-// import GradientCard from "../../../components/cards/GradientCard"
+
+import Tabletop from 'tabletop';
 
 export default class Second extends Component {
+  constructor() {
+    super()
+    this.state = {
+      data: []
+    }
+  }
+
+  componentDidMount() {
+    Tabletop.init({
+      key: '1L-ywhumD0TnbJVxhMK_zZ9hnPsSKA86xPen1XCqNuG8',
+      orderby: 'Posição',
+      reverse: false,
+      wanted: ['Despesas Mensais'],
+
+      callback: googleData => {
+        this.setState({
+          data: googleData,
+        })
+
+      },
+      simpleSheet: true
+    })
+  }
+
     render() {
+      const { data } = this.state
+      console.log(data[0])
         return (
             <Fragment>
             <Row>
-              <Colxx xxs="12">
+              <Colxx xxs="10">
                 <Breadcrumb heading="menu.second" match={this.props.match} />
-                <Separator className="mb-5" />
               </Colxx>
-            </Row>
-
-            <Row>
+              <Colxx xxs="2">                
+                {data.map(obj => {
+                  return (
+                    <span key={obj.id}>
+                      <h1>{obj.Total}</h1>
+                    </span>
+                  )
+                })}
+              </Colxx>
               <Colxx xxs="12">
-                <div className="react-contextmenu-wrapper">
-                  <div className="d-flex flex-row active card">
-                    <div className="pl-2 d-flex flex-grow-1 min-width-zero">
-                      <div className="card-body align-self-center d-flex flex-column flex-lg-row justify-content-between min-width-zero align-items-lg-center">
-                        <a className="w-40 w-sm-100" href="https://clear.com.br">
-                          <p className="list-item-heading mb-1 truncate">Clear Corretora</p>
-                        </a>
-                        <p className="mb-1 text-muted text-small w-15 w-sm-100">Desserts</p>
-                        <p className="mb-1 text-muted text-small w-15 w-sm-100">27.01.2019</p>
-                        <div className="w-15 w-sm-100">
-                          <span className="badge badge-secondary badge-pill">PROCESSED</span>
-                        </div>
-                      </div>
-                      <div className="custom-control custom-checkbox pl-1 align-self-center pr-4">
-                        <div className="item-check mb-0 custom-checkbox custom-control">
-                          <input type="checkbox" id="check_18" className="custom-control-input" />
-                            <label className="custom-control-label" for="check_18"></label>
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                </div>
+              <Separator className="mb-5" />
               </Colxx>
             </Row>
+            <Row>
+            <Colxx xs="6" className="mb-4">
+            <h2 className="text-center">Despesas Mensais</h2>
+            <div className="pl-2 d-flex flex-grow-1 min-width-zero">
+              <div className="card-body align-self-center d-flex flex-column flex-lg-row justify-content-between min-width-zero align-items-lg-center">
+              <p className="list-item-heading mb-1 truncate">
+                Nome
+              </p>
+              <p className="list-item-heading mb-1 truncate">
+                Vencimento
+              </p>
+              <p className="list-item-heading mb-1 truncate">
+                Valor
+              </p>
+              </div>
+            </div>
+
+              {
+                  data.map(obj => {
+                    return (
+                       <div key={obj.id} className="mb-4">
+
+                        <div className="d-flex flex-row active card">
+                          <div className={'pl-2 d-flex flex-grow-1 min-width-zero'}>
+                          <div className="card-body align-self-center d-flex flex-column flex-lg-row justify-content-between min-width-zero align-items-lg-center">
+                          <p className="list-item-heading mb-1 truncate">
+                            {obj.Nome}
+                          </p>
+                          <p className="mb-1 text-small w-15 w-sm-100">
+                            {obj.Vencimento}
+                          </p>
+                          <h2>
+                            <span className="badge badge-dark badge-pill">
+                              {obj.Valor}
+                            </span>
+                          </h2>
+                          </div>
+                        </div>
+                        </div>
+                      </div>
+                    )
+                  })
+                }
+                </Colxx>
+                <Colxx xxs="6" className="mb-4">
+                <h2 className="text-center">Links Úteis</h2>
+                </Colxx>
+              </Row>
           </Fragment>
         )
     }
