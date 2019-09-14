516abc74502959f0ea29c16f36ad75551a39992b
Léu Almeida <leo@webid.net.br>
2019-09-13 21:54:20 -0300

    Update stocks.js

--- src/views/app/gogo/stocks.js
+++ src/views/app/gogo/stocks.js
@@ -45,7 +45,7 @@ export default class Stocks extends Component {
               <Colxx xxs="2">                
               {data.map(obj => {
                   return (
-                    <span key={obj.Total} className={obj.Arrow === 'arrow-up-2' ? 'text-success' : 'text-warning'}>
+                    <span key={obj.Total} className={obj.Arrow === 'arrow-up-2' ? 'text-success' : 'text-danger'}>
                       <h1>
                         {obj.Total}
                         <Icon icon={obj.Arrow} />
